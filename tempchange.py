bfdce87f266665e7c6ca9dab1545914ee9b3e160
Yiting Lin <bestfriend830@gmail.com>
2021-10-02 10:29:13 +0800

    v2

--- tempchange.py
+++ tempchange.py
@@ -1,4 +1,4 @@
-temp_C = int(input('攝氏溫度為(C):'))
-temp_F = temp_C * 9/5 +32
+temp_C = float(input('請輸入攝氏溫度(C): '))
+temp_F = temp_C * 9 / 5 +32
 
-print('華氏溫度為(F)',temp_F)
+print('華氏溫度為(F): ', temp_F)
